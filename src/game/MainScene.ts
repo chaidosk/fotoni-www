22943ed1639deb79d6e9d3e6219d2aeeb304ff39
Chaidos Konstantinos <chaidosk>
2020-04-08 15:44:40 +0200

    Add levels and helpers

--- src/game/MainScene.ts
+++ src/game/MainScene.ts
@@ -1,57 +1,64 @@
-import Phaser from 'phaser'
+import Phaser from 'phaser';
+import {Level} from './Level';
+import LevelScene from './LevelScene';
 
 class MainScene extends Phaser.Scene {
-  tiles: Phaser.GameObjects.Group
+  levels: Level[]
+  currentLevelScene: LevelScene
+  currentLevelIndex: integer
+  onCompleteRegistered: boolean
   constructor() {
     super('MainScene');
     Phaser.Scene.call(this);
-  }
-
-  preload ()
-  {
-    this.load.spritesheet('tiles', 'assets/gridtiles.png', { frameWidth: 32, frameHeight: 32 });
+    this.levels = [
+      new Level("1", [
+        [0, 1, 1, 0],
+        [1, 0, 0, 1],
+        [1, 0, 0, 1],
+        [0, 1, 1, 0]
+      ]),
+      new Level("2", [
+        [1, 1, 1, 0],
+        [1, 0, 0, 1],
+        [0, 1, 0, 1],
+        [0, 1, 1, 0]
+      ]),
+      new Level("3", [
+        [0, 1, 1, 0],
+        [1, 0, 1, 1],
+        [1, 1, 0, 1],
+        [0, 1, 1, 0]
+      ]),
+    ];
+    this.currentLevelIndex = 0
   }
 
   create ()
   {
-    // for typescript linting
-    var adder : any = this.add
-
-    this.tiles = adder.group({
-      key: 'tiles',
-      frame: [ 41 ],
-      frameQuantity: 12 * 12
-    });
-
-    Phaser.Actions.GridAlign(this.tiles.getChildren(), {
-        width: 12,
-        height: 12,
-        cellWidth: 32,
-        cellHeight: 32,
-        x: 100,
-        y: 100
-    });
-
-    this.tiles.children.iterate(function (child: any) {
-      child.setInteractive();
-      child.setData("item", "tile")
-    });
-
-    this.input.on('gameobjectdown', this.onGameObjectDown, this);
+    this.currentLevelScene = new LevelScene(this.levels[0]);
+    this.scene.add(this.currentLevelScene.key, this.currentLevelScene, true);
+    this.onCompleteRegistered = false;
   }
 
-  onGameObjectDown(pointer: any, gameObject: any) {
-    if (gameObject.getData("item") == "tile") {
-        if (gameObject.frame.name == 41) {
-          gameObject.setFrame(14)
-        } else if (gameObject.frame.name == 14) {
-          gameObject.setFrame(41)
-        }
-    }
+  onLevelCompleted() {
+    this.scene.stop(this.currentLevelScene.key);
+    this.scene.remove(this.currentLevelScene.key);
+    this.currentLevelScene.events.removeAllListeners("completed")
+
+    this.currentLevelIndex += 1
+    this.currentLevelIndex = this.currentLevelIndex % this.levels.length;
+    this.currentLevelScene = new LevelScene(this.levels[this.currentLevelIndex])
+    this.scene.add(this.currentLevelScene.key, this.currentLevelScene, true);
+    this.onCompleteRegistered = false
   }
 
   update ()
   {
+    if (! this.onCompleteRegistered && this.currentLevelScene.events) {
+      this.currentLevelScene.events.on("completed", this.onLevelCompleted, this)
+      this.onCompleteRegistered = true;
+    }
+    
   }
 }
 
